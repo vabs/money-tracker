d4a002eebc3fead6fdc1f40d6ccd35c26cdd07c9
vabs <njoy.today@gmail.com>
2025-11-08 06:33:15 -0500

    fixing build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { TransactionProvider } from './contexts/TransactionContext';
+import { TransactionProvider } from './contexts/TransactionContext.jsx';
 import Home from './pages/Home';
 import ManageMoney from './pages/ManageMoney';
 
